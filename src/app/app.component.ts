d095b64719336be5f9d4906eea346738a1dc7aae
Benoit Denis <benoit.denis@spacebel.be>
2020-05-19 17:59:55 +0200

    app test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gui';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  createRecord() {
+    this.httpClient.post('http://localhost:8080/record', null).subscribe(() => alert('success'));
+  }
 }
